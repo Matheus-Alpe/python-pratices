4064b721144deb641442829a63838615a7bdcd64
Matheus-Alpe <matttalves@gmail.com>
2021-05-05 20:13:13 -0300

    add: open file

--- os.py
+++ os.py
@@ -10,3 +10,10 @@ from os.path import isfile, join
 my_path = "./os_test"
 onlyfiles = [file for file in listdir(my_path) if isfile(join(my_path, file)) and not file.endswith('.py')]
 print(onlyfiles)
+
+for nameFile in onlyfiles:
+    file = open("./os_test/"+nameFile, "r")
+    for line in file:
+        print(nameFile, '->', line)
+    
+    file.close()
