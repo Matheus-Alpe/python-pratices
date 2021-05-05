25f22121455946c512034845b8ceec6990d02972
Matheus-Alpe <matttalves@gmail.com>
2021-05-05 20:24:43 -0300

    refactor: change logic to read extension

--- os.py
+++ os.py
@@ -5,13 +5,13 @@
 #     print(file)
 
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, splitext
 
-my_path = "./os_test"
-onlyfiles = [file for file in listdir(my_path) if isfile(join(my_path, file)) and not file.endswith('.py')]
+my_path = "./os_test/"
+onlyfiles = [file for file in listdir(my_path) if isfile(join(my_path, file)) and splitext(file)[1] == '']
 onlyfiles = sorted(onlyfiles)
-print(onlyfiles)
 
+print(onlyfiles)
 
 for nameFile in onlyfiles:
     file = open("./os_test/"+nameFile, "r")
